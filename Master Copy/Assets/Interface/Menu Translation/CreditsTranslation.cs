245784c110259b3b5e6f82b5cc41c92e4cecbe68
Andrew <andrew.caruso03@gmail.com>
2016-04-22 11:39:10 -0400

    Added Credits scene
    Added and translated Credits scene.

---
+++ Master Copy/Assets/Interface/Menu Translation/CreditsTranslation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CreditsTranslation : MonoBehaviour {
+
+	public Text back;
+	public Text projectby;
+	public Text studioLead;
+	public Text scripter;
+	public Text artist;
+	public Text animationArtist;
+	public Text audioDesigner;
+	public Text localization;
+	public Text qaTester;
+	public Text specialThanks;
+
+	public static bool loaded = false;
+	
+	void Awake() {
+		if (!loaded) {
+			Localisation.Instance.LoadLanguage(Localisation.Language.English);
+			loaded = true;
+		}
+	}
+	
+	void OnEnable() {
+		back.text = Localisation.GetString("Back");
+        projectby.text = Localisation.GetString("Project By");
+        studioLead.text = Localisation.GetString("Studio Lead");
+        scripter.text = Localisation.GetString("Scripters");
+		artist.text = Localisation.GetString("Artist");
+		animationArtist.text = Localisation.GetString("Animation Artists");
+		audioDesigner.text = Localisation.GetString("Audio Designer");
+		localization.text = Localisation.GetString("Localization");
+		qaTester.text = Localisation.GetString("QA Testers");
+		specialThanks.text = Localisation.GetString("Special Thanks");
+    }
+}
---
+++ Master Copy/Assets/Scripts/CreditsText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsText : MonoBehaviour
+{
+	public float textSpeed = 5;
+
+	public Transform stopper;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (!(stopper.transform.position.y > 8))
+		{
+			transform.Translate (Vector3.up * textSpeed * Time.deltaTime);
+			stopper.Translate (Vector3.up * textSpeed * Time.deltaTime);
+		}
+	}
+}
